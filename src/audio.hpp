7a7cff3d9e7c8f45de2c4cd21e1bc2fbb2b9f963
eemorm <eemorm@gmail.com>
2025-12-24 11:34:28 -0600

    third batch of music - change later probably

--- src/audio.hpp
+++ src/audio.hpp
@@ -12,8 +12,13 @@ inline void loadAudio()
 {
     std::unique_ptr<sf::Music> m_Space = std::make_unique<sf::Music>();
     std::unique_ptr<sf::Music> m_sketch = std::make_unique<sf::Music>();
+    std::unique_ptr<sf::Music> m_sketch2 = std::make_unique<sf::Music>();
+    std::unique_ptr<sf::Music> m_daria = std::make_unique<sf::Music>();
+
     if (!m_Space->openFromFile("audio/music/SPACE.mp3")) { std::cout << "Failed to load audio file"; }
-    if (!m_sketch->openFromFile("audio/music/SKETCH.mp3")) { std::cout << "Failed to load audio file"; }
+    if (!m_sketch->openFromFile("audio/music/sketch.mp3")) { std::cout << "Failed to load audio file"; }
+    if (!m_sketch2->openFromFile("audio/music/sketch2.mp3")) { std::cout << "Failed to load audio file"; }
+    if (!m_daria->openFromFile("audio/music/dariacore3.mp3")) { std::cout << "Failed to load audio file"; }
 
     m_Space->setLoop(true);
     m_Space->setVolume(50.f);
@@ -21,8 +26,16 @@ inline void loadAudio()
     m_sketch->setLoop(true);
     m_sketch->setVolume(50.f);
 
+    m_sketch2->setLoop(true);
+    m_sketch2->setVolume(50.f);
+
+    m_daria->setLoop(true);
+    m_daria->setVolume(50.f);
+
     baccarat1.push_back(std::move(m_Space));
     baccarat1.push_back(std::move(m_sketch));
+    baccarat1.push_back(std::move(m_sketch2));
+    baccarat1.push_back(std::move(m_daria));
 }
 
 inline void playRandom(std::vector<std::unique_ptr<sf::Music>>& tracks)
